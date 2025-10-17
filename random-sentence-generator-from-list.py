8f07c8019e3a4e0f9ec01016be0fec514ef42a0f
Aaron Sood <theaaronsood@gmail.com>
2025-10-17 16:46:59 +1300

    Replace sentence generator with dice roller
    I uploaded the wrong file so I'm just replacing it

--- random-sentence-generator-from-list.py
+++ random-sentence-generator-from-list.py
@@ -1,8 +1,8 @@
-import random
-people = ["Jake", "Lily", "Brian"]
-fruits = ["apple", "banana", "cherry"]
-numbers = [1, 2, 3, 4, 5]
-fruit = random.choice(fruits)
-person = random.choice(people)
-number = random.choice(numbers)
-print(f"{person} ate {number} {fruit}s.")
+import random 
+while True: 
+  number = random.randint(1, 6)
+  print(f"The result is {number}.") 
+  answer = input("Would you like to roll again? Enter yes if you do: ") 
+  answer = answer.strip().lower()
+if answer != "yes": 
+  print("Stopping the dice roller.") break
